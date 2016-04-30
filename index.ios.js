7ccf4e8a024834713d4168c7e9965d8c288c9341
Dmitry <irvis@imega.ru>
2016-04-30 22:07:18 +0300

    remove folder build

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import App from './src/components/App'
 class lolo extends App {
     static defaultProps = {
         ...App.defaultProps,
-        instructions: '1Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+        instructions: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu or adb shell input keyevent 82',
     };
 }
 
